4c9a38d2473fdfe9ef6c280582e20aa2ee34ac32
paul guiselin <paul.guiselin@edu.devinci.fr>
2024-03-27 03:12:04 +0100

    Final commit

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,48 +1,173 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_NODE_PORT } from "../config";
-import { Value } from "../types";
+import {BASE_NODE_PORT} from "../config";
+import {Value, NodeState} from "../types";
+import {delay} from "../utils";
+import * as console from "console";
 
 export async function node(
-  nodeId: number, // the ID of the node
-  N: number, // total number of nodes in the network
-  F: number, // number of faulty nodes in the network
-  initialValue: Value, // initial value of the node
-  isFaulty: boolean, // true if the node is faulty, false otherwise
-  nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
-  setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
+    nodeId: number,
+    N: number,
+    F: number,
+    initialValue: Value,
+    isFaulty: boolean,
+    nodesAreReady: () => boolean,
+    setNodeIsReady: (index: number) => void
 ) {
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
 
-  // TODO implement this
-  // this route allows retrieving the current status of the node
-  // node.get("/status", (req, res) => {});
+  let currentNodeState: NodeState = {
+    killed: false,
+    x: null,
+    decided: null,
+    k: null,
+  }
+  let proposals: Map<number, Value[]> = new Map();
+  let votes: Map<number, Value[]> = new Map();
 
-  // TODO implement this
-  // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
+  // Route pour vérifier l'état d'un nœud
+  node.get("/status", (req, res) => {
+    const status = isFaulty ? "faulty" : "live";
+    const statusCode = isFaulty ? 500 : 200;
+    res.status(statusCode).send(status);
+  });
 
-  // TODO implement this
-  // this route is used to start the consensus algorithm
-  // node.get("/start", async (req, res) => {});
+  // Route pour obtenir l'état actuel d'un nœud
+  node.get("/getState", (req, res) => {
+    const { killed, x, decided, k } = currentNodeState;
+    res.status(200).send({ killed, x, decided, k });
+  });
 
-  // TODO implement this
-  // this route is used to stop the consensus algorithm
-  // node.get("/stop", async (req, res) => {});
+  node.get("/stop", (req, res) => {
+    if (!currentNodeState.killed) {
+      currentNodeState.killed = true;
+      res.status(200).send("killed");
+    } else {
+      res.status(200).send("already killed");
+    }
+  });
 
-  // TODO implement this
-  // get the current state of a node
-  // node.get("/getState", (req, res) => {});
+// Route pour recevoir les messages
+  node.post("/message", async (req, res) => {
+    let { k, x, messageType } = req.body; // Utilisation de 'let' pour 'x' pour permettre la réaffectation
 
-  // start the server
+    if (!isFaulty && !currentNodeState.killed) {
+
+      if (messageType === "propose") {
+        if (!proposals.has(k)) {
+          proposals.set(k, []);
+        }
+
+        let proposal = proposals.get(k);
+        if (proposal) {
+          proposal.push(x);
+
+          if (proposal.length >= (N - F)) {
+            const count0 = proposal.filter(el => el === 0).length;
+            const count1 = proposal.filter(el => el === 1).length;
+
+            if (count0 > (N / 2)) x = 0;
+            else if (count1 > (N / 2)) x = 1;
+            else x = "?";
+
+            // Envoie de la prop aux noeuds
+            for (let i = 0; i < N; i++) {
+              fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ k, x, messageType: "vote" }),
+              });
+            }
+          }
+        }
+      }
+      // Traitement pour le type de message "vote"
+      else if (messageType === "vote") {
+        // Ici on init le tableau de vote
+        if (!votes.has(k)) {
+          votes.set(k, []);
+        }
+        let vote = votes.get(k);
+        if (vote) {
+          vote.push(x);
+
+          // On checdk si le nbre de votes à atteint le seuil
+          if (vote.length >= (N - F)) {
+            console.log("vote", vote, "node :", nodeId, "k :", k);
+
+            // Ici o ncompte le nombre de vote
+            const count0 = vote.filter(el => el === 0).length;
+            const count1 = vote.filter(el => el === 1).length;
+
+            // On détermine le nouvel état du noeud
+            if (count0 >= F + 1) {
+              currentNodeState.x = 0;
+              currentNodeState.decided = true;
+            } else if (count1 >= F + 1) {
+              currentNodeState.x = 1;
+              currentNodeState.decided = true;
+            } else {
+              if (count0 + count1 > 0 && count0 > count1) {
+                currentNodeState.x = 0;
+              } else if (count0 + count1 > 0 && count0 < count1) {
+                currentNodeState.x = 1;
+              } else {
+                currentNodeState.x = Math.random() > 0.5 ? 0 : 1;
+              }
+              currentNodeState.k = k + 1;
+
+              // Nouvelle prop aux autres noeuds
+              for (let i = 0; i < N; i++) {
+                fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+                  method: 'POST',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({ k: currentNodeState.k, x: currentNodeState.x, messageType: "propose" }),
+                });
+              }
+            }
+          }
+        }
+      }
+    }
+    res.status(200).send("Message received and processed.");
+  });
+
+  // Route pour démarrer l'algorithme de consensus
+  node.get("/start", async (req, res) => {
+    while (!nodesAreReady()) {
+      await delay(5);
+    }
+    if (!isFaulty) {
+      currentNodeState.k = 1;
+      currentNodeState.x = initialValue;
+      currentNodeState.decided = false;
+      for (let i = 0; i < N; i++) {
+        fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ k: currentNodeState.k, x: currentNodeState.x, messageType: "propose" }),
+        });
+      }
+    }
+    else {
+      currentNodeState.decided = null;
+      currentNodeState.x = null;
+      currentNodeState.k = null;
+    }
+    res.status(200).send("Consensus algorithm started.");
+  });
+
+  // On launch le serveur
   const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
     console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
+        `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
     );
 
-    // the node is ready
+    // le noeud est prêt
     setNodeIsReady(nodeId);
   });
 
